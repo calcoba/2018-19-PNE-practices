9b6df9a9bec50c7bdeadd490bd2b84030185a8e9
calcoba <46930505+calcoba@users.noreply.github.com>
2019-01-29 17:23:12 +0100

    Count the number of each base presented in the DNA sequence

---
+++ P0-extra/CPLX2_count.py
@@ -0,0 +1,19 @@
+bases = "A", "C", "T", "G"
+count_a = 0
+count_c = 0
+count_t = 0
+count_g = 0
+counts = [count_a, count_c, count_t, count_g]
+with open("CPLX2.txt") as file:
+    dna_chain = file.read()
+    dna_chain = dna_chain.split("\n")
+    dna_chain_final = list()
+    for line in dna_chain:
+        if not line.startswith(">"):
+           for i in range(len(bases)):
+                counts[i] += line.count(bases[i])
+
+    for element in range(len(bases)):
+        print("Letter", bases[element], "is repeated", counts[element], "times")
+
+
