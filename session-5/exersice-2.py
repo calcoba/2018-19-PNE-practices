c7d78cb86ab7d5abece0aee54f0e95a9f1ada788
calcoba <calcobanunez@gmail.com>
2019-02-06 17:44:57 +0100

    No errors found

--- session-5/exersice-2.py
+++ session-5/exersice-2.py
@@ -18,10 +18,9 @@ for s in sequences:
     bases_dic = count_bases(s)
     sn += 1
 
-
     # Calculate the total sequence length
     tl = len(s)
-    print("Sequence {} is {} bases in length".format(sn, tl) )
+    print("Sequence {} is {} bases in length".format(sn, tl))
     for key, value in bases_dic.items():
         if tl > 0:
             perc = round(100.0 * value / tl, 1)
