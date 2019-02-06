02d9b9d92f6b45e6c2cf3dc7b9ca06030ee8c99d
calcoba <calcobanunez@gmail.com>
2019-02-06 17:20:27 +0100

    No errors found

---
+++ session-5/exersice-2.py
@@ -0,0 +1,33 @@
+def count_bases(seq):
+    """Counting the number of each base in the sequence"""
+
+    n_bases = {}
+    b_set = set(seq)
+    for i in b_set:
+        n_bases.update({i: seq.count(i)})
+
+    return n_bases
+
+
+s1 = input("Enter the sequence 1: ")
+s2 = input("Enter the sequence 2: ")
+
+sequences = s1, s2
+sn = 0
+for s in sequences:
+    bases_dic = count_bases(s)
+    sn += 1
+
+
+    # Calculate the total sequence length
+    tl = len(s)
+    print("Sequence {} is {} bases in length".format(sn, tl) )
+    for key, value in bases_dic.items():
+        if tl > 0:
+            perc = round(100.0 * value / tl, 1)
+        else:
+            perc = 0
+        print("Base {}".format(key))
+        print("  Counter: {}".format(value))
+        print("  Percentage: {}".format(perc))
+    print()
