a316138e08af41470189984327b83a9a7815ec0e
calcoba <calcobanunez@gmail.com>
2019-02-12 16:08:55 +0100

    Try

---
+++ session-7/file_read.py
@@ -0,0 +1,13 @@
+# Example for reading a file located
+# In our local filesystem
+
+NAME = "mynotes.txt"
+
+# Open the file
+myfile = open(NAME, 'r')
+
+print("File openned: {}".format(myfile.name))
+
+contents = myfile.read()
+
+print("The file contents are: {}".format(contents))
