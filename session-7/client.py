c63784d2bc49196e27e4dbcc0940973bfd1c951e
calcoba <calcobanunez@gmail.com>
2019-02-13 17:19:48 +0100

    Client configuration

---
+++ session-7/client.py
@@ -0,0 +1,19 @@
+# Programming our first client
+import socket
+while True:
+    # Create a socket for communicating with the server
+    s = socket.socket(socket. AF_INET, socket.SOCK_STREAM)
+
+    print("Socket created")
+
+    PORT = 8080
+    IP = "212.128.253.112"
+    # Connect to server
+    s.connect((IP, PORT))
+
+    # Encode convert a string to bytes
+    s.send(str.encode(input("Write a message: ")))
+
+    msg = s.recv(2048).decode("utf-8")
+    print("MESSAGE FROM THE SERVER")
+    print(msg)
