164efd8ec393aa05d21ce8d118ce655d0cfa9a2a
calcoba <46930505+calcoba@users.noreply.github.com>
2019-02-15 11:02:50 +0100

    Seq imported, reverse and complement sent to the server

--- P2/client-seq.py
+++ P2/client-seq.py
@@ -1,5 +1,6 @@
 # Programming our first client
 import socket
+from Seq import Seq
 
 while True:
     # Create a socket for communicating with the server
@@ -7,13 +8,21 @@ while True:
 
     print("Socket created")
 
-    PORT = 8081
-    IP = "212.128.253.113"
+    PORT = 8080
+    IP = "192.168.56.1"
     # Connect to server
     s.connect((IP, PORT))
 
+    # Ask the user for a sequence
+    msg = Seq(input("Write a sequence: "))
+
+    # Getting the reverse and the complement of the sequence
+    s_reverse = msg.reverse().strbases
+    s_comp = msg.complement().strbases
+    new_seq = s_reverse, s_comp
+
     # Encode convert a string to bytes
-    s.send(str.encode(input("Write a sequence: ")))
+    s.send(str.encode(str(new_seq)))
 
     msg = s.recv(2048).decode("utf-8")
     print("MESSAGE FROM THE SERVER")
