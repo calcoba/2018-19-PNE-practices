bfe3f0b1e700ac8aad21d57d0e6f5b4e3d20793f
calcoba <46930505+calcoba@users.noreply.github.com>
2019-02-21 19:07:25 +0100

    Updated to send an empty message

--- P3/client.py
+++ P3/client.py
@@ -1,17 +1,21 @@
 import socket
 
 # SERVER IP, PORT
-PORT = 8080
-IP = "212.128.253.110"
+PORT = 8000
+IP = "127.0.0.1"
 activity = True
 
 
-msg = """EXIT"""
+msg = input(">")
+msg = msg.replace(" ", "\n")
+if not msg:
+    msg = "empty"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # establish the connection to the Server (IP, PORT)
 s.connect((IP, PORT))
 # Send the request message to the server
 s.send(str.encode(msg))
+print("conected")
 
 # Receive the servers respoinse
 response = s.recv(2048).decode()
