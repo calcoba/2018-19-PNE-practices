3b368a7537edd4da2362f1dbcd31b5bc25e30db3
calcoba <calcobanunez@gmail.com>
2019-02-26 15:16:21 +0100

    ERROR for seq implemented

--- P3/client.py
+++ P3/client.py
@@ -1,27 +1,27 @@
 import socket
 
 # SERVER IP, PORT
-PORT = 8000
-IP = "127.0.0.1"
+PORT = 8002
+IP = "212.128.253.106"
 activity = True
 
+while activity:
+    msg = input(">")
+    msg = msg.replace(" ", "\n")
+    if not msg:
+        msg = "empty"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
+    # Send the request message to the server
+    s.send(str.encode(msg))
+    print("conected")
 
-msg = input(">")
-msg = msg.replace(" ", "\n")
-if not msg:
-    msg = "empty"
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# establish the connection to the Server (IP, PORT)
-s.connect((IP, PORT))
-# Send the request message to the server
-s.send(str.encode(msg))
-print("conected")
+    # Receive the servers respoinse
+    response = s.recv(2048).decode()
 
-# Receive the servers respoinse
-response = s.recv(2048).decode()
-
-# Print the server's response
-print("Response: {}".format(response))
+    # Print the server's response
+    print("Response: {}".format(response))
 
 
-s.close()
+    s.close()
