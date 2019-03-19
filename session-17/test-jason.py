4065f2fc0508e247fb78bcfe2845e579739d7efe
calcoba <calcobanunez@gmail.com>
2019-03-19 16:38:06 +0100

    PhoneNumber Key updated

--- session-17/test-jason.py
+++ session-17/test-jason.py
@@ -16,7 +16,7 @@ print(person['Firstname'], person['Lastname'])
 termcolor.cprint("Age: ", 'green', end='')
 print(person['age'])
 
-for i, num in enumerate(person['phonenumber']):
+for i, num in enumerate(person['phoneNumber']):
     termcolor.cprint(" Phone {}:".format(i))
 
     termcolor.cprint("     Type: ", 'red', end='')
