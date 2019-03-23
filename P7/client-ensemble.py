94d13ac9a8ddf3e919de50533ea5b1cce2e110df
calcoba <46930505+calcoba@users.noreply.github.com>
2019-03-23 21:06:25 +0100

    Commenting the code

--- P7/client-ensemble.py
+++ P7/client-ensemble.py
@@ -1,6 +1,8 @@
-import requests, sys, json
+import requests
+import sys
 from SeqP7 import Seq
 
+# Connecting to the ensemble database to get the info
 server = "http://rest.ensembl.org"
 ext = "/sequence/id"
 headers = {"Content-Type": "application/json", "Accept": "application/json"}
@@ -11,7 +13,11 @@ if not r.ok:
     sys.exit()
 
 decoded = r.json()
+
+# Get the seq from the received message and p convert it to an object
 seq = Seq(decoded[-1]['seq'])
+
+# Do the requested calculation
 bases = set(seq.strbases)
 perc_bases = {}
 count_bases = {}
@@ -20,8 +26,9 @@ total_len = seq.len()
 for base in bases:
     perc_bases.update({base: seq.perc(base)})
     count_bases.update({base: seq.count(base)})
-
 max_perc = max(perc_bases.items(), key=lambda x: x[1])
+
+# Printing the results
 print("The FRAT1 gen has {} bases.".format(total_len))
 print("There are {} T bases in the FRAT1 gene.".format(count_bases["T"]))
 print("The most popular base in the FRAT1 gene is {} and it represents the {}%.".format(max_perc[0], max_perc[1]))
